d1df0a9e9deb71e8177a8ad069169d54d8fc2005
Igor Ambonatti <contatoambonatti@gmail.com>
2024-03-29 13:26:18 -0300

    wallet hook

--- src/components/SwapSection.tsx
+++ src/components/SwapSection.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import Web3 from "web3";
+import React, { useState } from "react";
 import { Contract } from "web3-eth-contract";
-import bittexAbi from "../bittex.json";
 import { truncateString } from "@/utils/truncateString";
-
-const contractAddress = "0xCA3f028bB42C01036DbFc5fF20a3e6C58Fe0Acf1";
+import useWallet from "@/hooks/useWallet";
 
 const initialWalletState = {
   metaMaskConnected: false,
@@ -26,48 +23,13 @@ const initialSwapState = {
 type SwapState = typeof initialSwapState;
 
 const SwapSection: React.FC = () => {
-  const [web3, setWeb3] = useState<Web3 | null>(null);
-  const [walletState, setWalletState] =
-    useState<WalletState>(initialWalletState);
+  const { web3, walletState, setWalletState } = useWallet();
+  useState<WalletState>(initialWalletState);
   const [swapState, setSwapState] = useState<SwapState>(initialSwapState);
 
   const { metaMaskConnected, walletAddress, contract, connectLoading } =
     walletState;
   const { loading, result, error } = swapState;
-
-  useEffect(() => {
-    loadWeb3();
-  }, []);
-
-  const loadWeb3 = async () => {
-    try {
-      if (typeof window.ethereum !== "undefined") {
-        const web3Instance = new Web3(window.ethereum);
-        setWeb3(web3Instance);
-
-        const deployedContract = new web3Instance.eth.Contract(
-          bittexAbi as any,
-          contractAddress
-        );
-        setWalletState((prevWalletState) => ({
-          ...prevWalletState,
-          contract: deployedContract,
-        }));
-        const accounts = await window.ethereum.request({
-          method: "eth_accounts",
-        });
-        if (accounts.length > 0) {
-          setWalletState((prevWalletState) => ({
-            ...prevWalletState,
-            metaMaskConnected: true,
-            walletAddress: accounts[0],
-          }));
-        }
-      }
-    } catch (error) {
-      console.error("Failed to load Web3:", error);
-    }
-  };
 
   const connectMetaMask = async () => {
     try {
---
+++ src/hooks/useWallet.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import Web3 from "web3";
+import { Contract } from "web3-eth-contract";
+import bittexAbi from "../bittex.json";
+
+const contractAddress = "0xCA3f028bB42C01036DbFc5fF20a3e6C58Fe0Acf1";
+
+const useWallet = () => {
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+  const [walletState, setWalletState] = useState({
+    metaMaskConnected: false,
+    walletAddress: null as string | null,
+    contract: null as Contract<any> | null,
+    connectLoading: false,
+  });
+
+  const loadWeb3AndContract = async () => {
+    try {
+      if (typeof window.ethereum !== "undefined") {
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+
+        const deployedContract = new web3Instance.eth.Contract(
+          bittexAbi as any,
+          contractAddress
+        );
+
+        if (accounts.length > 0) {
+          setWalletState((prevWalletState) => ({
+            ...prevWalletState,
+            metaMaskConnected: true,
+            walletAddress: accounts[0],
+            contract: deployedContract,
+          }));
+        }
+      }
+    } catch (error) {
+      console.error("Failed to load Web3 and contract:", error);
+    }
+  };
+
+  useEffect(() => {
+    loadWeb3AndContract();
+  }, []);
+
+  return { web3, walletState, setWalletState };
+};
+
+export default useWallet;
